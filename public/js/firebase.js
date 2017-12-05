64e4052bc83e06109473201db015e955942a4981
Felipe Guimarães <felipeemg@gmail.com>
2017-12-05 21:12:04 -0200

    loja responsiva

---
+++ public/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBAYAxGF9zI7SnJitYY-u92UEmt0-8GqN0",
+  authDomain: "improvavel-loja.firebaseapp.com",
+  databaseURL: "https://improvavel-loja.firebaseio.com",
+  projectId: "improvavel-loja",
+  storageBucket: "improvavel-loja.appspot.com",
+  messagingSenderId: "411155534834"
+};
+firebase.initializeApp(config);

a58fbb79d784acea75502081841a08adefbdff02
Felipe Guimarães <fguimaraes@granadophebo.com.br>
2018-03-08 15:12:26 -0300

    add Firebase

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,10 +1,9 @@
-// Initialize Firebase
 var config = {
-  apiKey: "AIzaSyBAYAxGF9zI7SnJitYY-u92UEmt0-8GqN0",
-  authDomain: "improvavel-loja.firebaseapp.com",
-  databaseURL: "https://improvavel-loja.firebaseio.com",
-  projectId: "improvavel-loja",
-  storageBucket: "improvavel-loja.appspot.com",
-  messagingSenderId: "411155534834"
-};
-firebase.initializeApp(config);
+    apiKey: "AIzaSyBqx-m3K9npJ-YPoikT-Btg7TS8t2NrHw4",
+    authDomain: "cv-felipe.firebaseapp.com",
+    databaseURL: "https://cv-felipe.firebaseio.com",
+    projectId: "cv-felipe",
+    storageBucket: "cv-felipe.appspot.com",
+    messagingSenderId: "883754756950"
+  };
+  firebase.initializeApp(config);
